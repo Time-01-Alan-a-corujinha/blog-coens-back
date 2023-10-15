2f32d73c7ad2052f49abc2daf13eff0b083b8ff6
Leandro Girotto Cabrini Junior <leandrogirottocabrini@gmail.com>
2023-10-15 19:38:24 -0300

    Leandrinho Bonitão

---
+++ src/api/post-approval/controllers/post-approval.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * post-approval controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::post-approval.post-approval');
