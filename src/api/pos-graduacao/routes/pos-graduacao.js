6b385b1b45b4e1c502a5dcb4801b8940d54f0ada
Júlia Cardozo <ccardozojulia@gmail.com>
2023-11-06 11:01:00 -0300

    Implementado componente menu pos-graduacao

---
+++ src/api/pos-graduacao/routes/pos-graduacao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pos-graduacao router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::pos-graduacao.pos-graduacao');
