cfc7d98310d7d86c9096e110fe1b0935fe34cf87
Júlia Cardozo <ccardozojulia@gmail.com>
2023-11-13 22:47:19 -0300

    fix: Grupos de pesquisa permitindo criar rota

---
+++ src/api/atividades-e-producao-projetos-de-extensao/controllers/atividades-e-producao-projetos-de-extensao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * atividades-e-producao-projetos-de-extensao controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::atividades-e-producao-projetos-de-extensao.atividades-e-producao-projetos-de-extensao');
---
+++ src/api/atividades-e-producao-projetos-de-extensao/routes/atividades-e-producao-projetos-de-extensao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * atividades-e-producao-projetos-de-extensao router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::atividades-e-producao-projetos-de-extensao.atividades-e-producao-projetos-de-extensao');
