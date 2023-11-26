5a121e6974c06c4142bbca9bcd071d0492071d62
Leandro Girotto Cabrini Junior <leandrogirottocabrini@gmail.com>
2023-11-26 19:32:40 -0300

    Scrum-161

--- src/api/post-approval/controllers/post-approval.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * post-approval controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::post-approval.post-approval');
